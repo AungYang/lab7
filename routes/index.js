47a1799cfdde8e3677a77c86ad5dc855dde5975f
Aung Yang <aungkyang622@gmail.com>
2021-02-18 10:54:13 -0800

    lab 7

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
-  	response.render('index', projects);
+    data['viewAlt'] = false;
+	response.render('index', projects);
 };
+
+exports.viewAlt = function(req, res) {
+	data['viewAlt'] = true;
+	res.render('index', data);
+}
+
